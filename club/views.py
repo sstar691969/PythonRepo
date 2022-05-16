ef506a4a5dc6a2f48bfac0878a2f9998bfd3e0cc
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2022-05-16 13:52:00 -0700

    4 committs

--- club/views.py
+++ club/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from .models import Meeting 
 def index(request):
-    return render(request, 'tech/index.html')
-    
+    return render(request, 'club/index.html')
+
+def meeting(request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'club/meeting.html', {'meeting_list': meeting_list})  
+
