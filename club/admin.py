58190589fd856ce04691b83a7cc4678e545b92f0
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2022-05-19 17:51:58 -0700

    added meeting.py plus index.py and admin.py

--- club/admin.py
+++ club/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Meeting, Meeting_Minutes, Resource, Event
+from .models import Meeting, Meeting_Minutes, Resource, Event
 # Register your models here.
 
 admin.site.register(Meeting)
